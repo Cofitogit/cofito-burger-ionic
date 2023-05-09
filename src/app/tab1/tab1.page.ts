1b5e0059a587dd77c20a7ab2f17414addfcd7ef8
Cofitogit <kikelin32@gmail.com>
2023-05-09 14:06:11 -0300

    new scss

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private animationCtrl: AnimationController) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      const logoImg: HTMLElement | null = document.querySelector('.slide-img');
+      if (logoImg !== null) {
+        // Verificar si no es nulo
+        const logo: Animation = this.animationCtrl
+          .create()
+          .addElement(logoImg)
+          .duration(2300)
+          .iterations(1)
+          .keyframes([
+            { offset: 0, transform: 'translate(0)' },
+            { offset: 0.25, transform: 'translate(-5%) rotate(3deg)' },
+            { offset: 0.5, transform: 'translate(5%) rotate(-3deg)' },
+            { offset: 1, transform: 'translate(0)' },
+          ]);
+  
+        logo.play();
+      }
+    }, 4000)
+  }
 
 }
